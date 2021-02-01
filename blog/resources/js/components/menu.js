045ec250892f0d4888bf502a346f9f9765a48761
webatozexpert <webatozexpert@gmail.com>
2021-02-01 23:47:45 +0600

    Revert "Revert "Revert "delete"""
    This reverts commit a0f083ff1cbbe1747a422dd8caf1604371325242.

--- blog/resources/js/components/menu.js
+++ blog/resources/js/components/menu.js
@@ -1,24 +1,26 @@
 import React, {Component, Fragment} from 'react';
-import {Nav, Navbar} from "react-bootstrap";
-import "../../css/style.css";
-import NavLogo from '../../images/logo.svg'
-import {NavLink} from "react-router-dom";
+import Navbar from "react-bootstrap/cjs/Navbar";
+import Nav from "react-bootstrap/cjs/Nav";
+
+
+
 class Menu extends Component {
     render() {
         return (
             <Fragment>
-                <Navbar className="fixed-top" variant="light" expand="lg">
-                    <Navbar.Brand href="#home"><img className="nav-logo" src={NavLogo}/></Navbar.Brand>
+                <Navbar bg="dark" variant="dark" expand="lg">
+                    <Navbar.Brand href="#home">Laravel-React</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                            <NavLink  className="nav-font" to="/"  href="#home">Home</NavLink>
-                            <NavLink className="nav-font"  to="/courseplan" href="#link">Course Plan</NavLink>
-                            <NavLink className="nav-font"  to="/registration" href="#link">Registration</NavLink>
-                            <NavLink className="nav-font" to="/freeclass"   href="#link">Free Class</NavLink>
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+
                         </Nav>
+
                     </Navbar.Collapse>
                 </Navbar>
+
             </Fragment>
         );
     }
